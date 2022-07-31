5581b69f7dc60731a44ac57b43df5fcd1bc33b60
horden <horden316@gmail.com>
2022-07-31 10:29:38 +0800

    try some function

--- Mavlink/takeoff.py
+++ Mavlink/takeoff.py
@@ -8,8 +8,9 @@ Created on Sun Jul 10 15:19:24 2022
 import time
 from pymavlink import mavutil
 
-the_connection = mavutil.mavlink_connection("/dev/cu.usbmodem01")
+the_connection = mavutil.mavlink_connection("/dev/cu.usbmodem141101")
 
+#the_connection = mavutil.mavlink_connection("tcp:192.168.4.1:6789")
 
 #the_connection.reboot_autopilot()
 
@@ -19,7 +20,46 @@ print("Heartbeat from system (system %u component %u)" %
     (the_connection.target_system, the_connection.target_component))
 
 
+the_connection.arducopter_arm()
 
+#測試ARM的
+print("Waiting for the vehicle to arm")
+the_connection.motors_armed_wait()
+print('Armed!')
+
+#Takeoff
+the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                                     mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 10)
+
+msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
+print(msg)
+
+
+the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                                     mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE, 0, 1, 0, 0, 0, 0, 0, 0)
+
+#33 for GLOBAL_POSITION_INT
+the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                                     mavutil.mavlink.MAV_CMD_REQUEST_MESSAGE, 0, 33, 0, 0, 0, 0, 0, 0)
+
+
+
+
+
+'''
+
+
+#command_long_send(target_system, target_component, command, confirmation, param1, param2, param3, param4, param5, param6, param7) 他寫在message #76的格式
+
+#Takeoff指令
+the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                                     mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 10)
+
+msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
+print(msg)
+
+
+#ARM
 the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
                                      mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
 
@@ -27,13 +67,11 @@ msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
 print(msg)
 
 
+#測試ARM的
+print("Waiting for the vehicle to arm")
+the_connection.motors_armed_wait()
+print('Armed!')
 
-'''
-the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-                                     mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 10)
-
-msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
-print(msg)
 
 
 
@@ -42,10 +80,12 @@ while True:
         type='LOCAL_POSITION_NED', blocking=True)
     print(msg)
 
-
+#用GPS取得的高度（但其實裡面很多跟高度無關的值？）
 while True:
     msg = the_connection.recv_match(type='ATTITUDE', blocking=True)
     print(msg)
+
+
 
 # Request all parameters
 the_connection.mav.param_request_list_send(
@@ -63,12 +103,7 @@ while True:
 
 
 
-while True:
-    yaw=the_connection.recv_match(type='COMMAND_ACK', blocking=True)
-    yaw = the_connection.messages['MAV_SYS_STATUS_SENSOR_YAW_POSITION'].alt
-    print(yaw)
-
-
+#把整個recv_match讀到的.to_dict()後篩出yaw
 while True:
     try:
         infordict = the_connection.recv_match().to_dict()
@@ -79,27 +114,11 @@ while True:
     
 
 
-
-the_connection.wait_heartbeat()
-print("Heartbeat from system (system %u component %u)" %
-      (the_connection.target_system, the_connection.target_component))
-
-
+#看起來不能這樣玩
 while True:
     yaw=the_connection.recv_match(type='COMMAND_ACK', blocking=True)
     yaw = the_connection.messages['MAV_SYS_STATUS_SENSOR_YAW_POSITION'].alt
     print(yaw)
-
-
-
-while True:
-    try:
-        infordict = the_connection.recv_match().to_dict()
-        print(infordict["yaw"])
-
-    except:
-        pass
-    time.sleep(0.1)
 
 
 
@@ -125,17 +144,5 @@ except:
     
     
 
-
-the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-                                     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
-
-msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
-print(msg)
-
-the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
-                                     mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 10)
-
-msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
-print(msg)
 
 '''
