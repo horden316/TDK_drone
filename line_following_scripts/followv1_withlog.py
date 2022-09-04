f868c5e99fc28da6ce58c815140862b7ad3a07d3
horden <horden316@gmail.com>
2022-09-04 17:00:45 +0800

    feat: followv1 with video log first commit

---
+++ line_following_scripts/followv1_withlog.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python
+#2022/8/17 update
+#adding arm function, takeoff safety
+#line following using contours method
+import cv2
+import numpy as np
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
+from pymavlink import mavutil # Needed for command message definitions
+import time
+import math
+
+#全域變數
+
+#screen resolution
+X=160
+Y=120
+center =  (int(X/2),int(Y/2))
+center_x = int(X/2)
+center_y = int(Y/2)
+cross_size = 5
+cap = cv2.VideoCapture(0)
+
+#VideoWriter
+fourcc = cv2.VideoWriter_fourcc('M','P','4','V') #指定影像編碼方式
+out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (1280,  960))
+
+#建立空frame2
+blank_width=1280
+blank_height=960
+
+cap.set(3, X)
+cap.set(4, Y)
+connection_string = '/dev/ttyACM1'
+print('Connecting to vehicle on: %s' % connection_string)
+vehicle = connect(connection_string, wait_ready=True, baud=115200)
+
+def WriteText(frame, text1):
+    cv2.putText(frame, text1, (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.1, (201, 194, 9), 1, cv2.LINE_AA)
+
+def arm():
+    print("Basic pre-arm checks")
+    vehicle.mode = VehicleMode("GUIDED_NOGPS")
+    vehicle.armed = True
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        vehicle.armed = True
+        time.sleep(1)
+    print("armed")
+
+def arm_and_takeoff_nogps(aTargetAltitude,DEFAULT_TAKEOFF_THRUST = 0.55,SMOOTH_TAKEOFF_THRUST = 0.55,limit_time=5):
+    """
+    Arms vehicle and fly to aTargetAltitude without GPS data.
+    """
+    print("Basic pre-arm checks")
+    # Don't let the user try to arm until autopilot is ready
+    # If you need to disable the arming check,
+    # just comment it with your own responsibility.
+
+    '''
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
+        time.sleep(1)
+    '''
+
+
+    print("Arming motors")
+    # Copter should arm in GUIDED_NOGPS mode
+    vehicle.mode = VehicleMode("GUIDED_NOGPS")
+    vehicle.armed = True
+
+    
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        vehicle.armed = True
+        time.sleep(1)
+
+
+    print("Taking off!")
+
+    thrust = DEFAULT_TAKEOFF_THRUST
+    start = time.time()
+    while True:
+        current_altitude = vehicle.rangefinder.distance
+        print(" Altitude: %f  Desired: %f" %
+              (current_altitude, aTargetAltitude))
+        if time.time() - start > limit_time:
+            print("take off timeout")
+            print("change mode to landing")
+            vehicle.mode = VehicleMode("LAND")
+            time.sleep(1)
+            while True:
+                print("vehicle emergency landing: open controller")
+        if current_altitude >= aTargetAltitude: # Trigger just below target alt.
+            print("Reached target altitude")
+            break
+        elif current_altitude >= aTargetAltitude:
+            thrust = SMOOTH_TAKEOFF_THRUST
+            print("thrust set to SMOOTH")
+        set_attitude(thrust = thrust)
+        time.sleep(0.2)
+
+def send_attitude_target(roll_angle = 0.0, pitch_angle = 0.0,
+                         yaw_angle = None, yaw_rate = 0.0, use_yaw_rate = False,
+                         thrust = 0.5):
+    """
+    use_yaw_rate: the yaw can be controlled using yaw_angle OR yaw_rate.
+                  When one is used, the other is ignored by Ardupilot.
+    thrust: 0 <= thrust <= 1, as a fraction of maximum vertical thrust.
+            Note that as of Copter 3.5, thrust = 0.5 triggers a special case in
+            the code for maintaining current altitude.
+    """
+    if yaw_angle is None:
+        # this value may be unused by the vehicle, depending on use_yaw_rate
+        yaw_angle = vehicle.attitude.yaw
+    # Thrust >  0.5: Ascend
+    # Thrust == 0.5: Hold the altitude
+    # Thrust <  0.5: Descend
+    msg = vehicle.message_factory.set_attitude_target_encode(
+        0, # time_boot_ms
+        1, # Target system
+        1, # Target component
+        0b00000000 if use_yaw_rate else 0b00000100,
+        to_quaternion(roll_angle, pitch_angle, yaw_angle), # Quaternion
+        0, # Body roll rate in radian
+        0, # Body pitch rate in radian
+        math.radians(yaw_rate), # Body yaw rate in radian/second
+        thrust  # Thrust
+    )
+    vehicle.send_mavlink(msg)
+
+def set_attitude(roll_angle = 0.0, pitch_angle = 0.0,
+                 yaw_angle = None, yaw_rate = 0.0, use_yaw_rate = False,
+                 thrust = 0.5, duration = 0):
+    """
+    Note that from AC3.3 the message should be re-sent more often than every
+    second, as an ATTITUDE_TARGET order has a timeout of 1s.
+    In AC3.2.1 and earlier the specified attitude persists until it is canceled.
+    The code below should work on either version.
+    Sending the message multiple times is the recommended way.
+    """
+    send_attitude_target(roll_angle, pitch_angle,
+                         yaw_angle, yaw_rate, False,
+                         thrust)
+
+    start = time.time()
+    while time.time() - start < duration:
+        send_attitude_target(roll_angle, pitch_angle,
+                             yaw_angle, yaw_rate, False,
+                             thrust)
+        time.sleep(0.1)
+        
+    # Reset attitude, or it will persist for 1s more due to the timeout
+    send_attitude_target(0, 0,
+                         0, 0, True,
+                         thrust)
+
+def to_quaternion(roll = 0.0, pitch = 0.0, yaw = 0.0):
+    """
+    Convert degrees to quaternions
+    """
+    t0 = math.cos(math.radians(yaw * 0.5))
+    t1 = math.sin(math.radians(yaw * 0.5))
+    t2 = math.cos(math.radians(roll * 0.5))
+    t3 = math.sin(math.radians(roll * 0.5))
+    t4 = math.cos(math.radians(pitch * 0.5))
+    t5 = math.sin(math.radians(pitch * 0.5))
+
+    w = t0 * t2 * t4 + t1 * t3 * t5
+    x = t0 * t3 * t4 - t1 * t2 * t5
+    y = t0 * t2 * t5 + t1 * t3 * t4
+    z = t1 * t2 * t4 - t0 * t3 * t5
+
+    return [w, x, y, z]
+
+def distanceCalculate(p1, p2):
+    """p1 and p2 in format (x1,y1) and (x2,y2) tuples"""
+    dis = ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
+    return dis
+
+##############主程式##############
+time.sleep(2)
+print("takeoff")
+arm_and_takeoff_nogps(0.6)
+set_attitude(thrust = 0.5,duration=2)
+while True:
+    ret, frame = cap.read()
+    #frame = cv2.imread('./webcam/opencv_frame_0.png')
+    low_b = np.uint8([255,255,255])
+    high_b = np.uint8([50,50,50])
+    mask = cv2.inRange(frame, high_b, low_b)
+    remask = cv2.bitwise_not(mask)
+    #apply erosion
+    kernel = np.ones((3,3), np.uint8)
+    erosion = cv2.erode(remask, kernel, iterations = 1)
+
+    contours, hierarchy = cv2.findContours(erosion, 1, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.line(frame, (center_x, center_y-cross_size), (center_x, center_y+cross_size), (0, 0, 255), 1)
+    cv2.line(frame, (center_x-cross_size, center_y), (center_x+cross_size, center_y), (0, 0, 255), 1)
+    start=time.time()
+    if time.time() - start > 20:
+        print("Setting LAND mode...")
+        vehicle.mode = VehicleMode("LAND")
+        time.sleep(1)
+        break
+    if len(contours) > 0 :
+        c = max(contours, key=cv2.contourArea)
+        M = cv2.moments(c)
+        blackbox = cv2.minAreaRect(c)
+        (x_min, y_min), (w_min, h_min), angle = blackbox
+        box = cv2.boxPoints(blackbox)
+        box = np.int0(box)
+        cv2.drawContours(frame, [box], 0, (0, 0, 255), 3)
+        if angle < -45:
+            angle = 90 + angle
+        if w_min < h_min and angle > 0:
+            angle = (90 - angle) * -1
+        if w_min > h_min and angle < 0:
+            angle = 90 + angle
+        print("Angle:" + str(angle))
+        cv2.putText(frame, "Angle: " + str(angle), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (201, 194, 9), 1,
+                        cv2.LINE_AA)
+        
+        if M["m00"] !=0 :
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            print("X : "+str(cx)+" Y : "+str(cy))
+            x_distance=center[0]-cx
+            if x_distance > 10 :
+                print("Roll right")
+                set_attitude(roll_angle = -5, thrust = 0.5)
+            elif x_distance < -10 and cx > 40 :
+                print("Roll left")
+                set_attitude(roll_angle = 5, thrust = 0.5)
+            else:
+                print("Pitch Forward")
+                set_attitude(pitch_angle = -5, thrust = 0.5)
+            
+            
+            
+            cv2.circle(frame, (cx,cy), 5, (0,0,255), -1)
+            #centroid line
+            cv2.line(frame,  center, (cx,cy), (0,255,255), 1)
+            #BGR
+            cv2.line(frame,  center, (cx,cy), (0,255,255), 1)
+            #distance = distanceCalculate(center, (cx,cy))
+            WriteText(frame,text1="x_distance: " + str(x_distance) )
+
+            
+    else :
+        print("I don't see the line")
+    #cv2.drawContours(frame, c, -1, (0,255,0), 5)
+    cv2.imshow("Mask",remask)
+    cv2.imshow("Erosion",erosion)
+    cv2.imshow("Frame",frame)
+    out.write(frame)
+    if cv2.waitKey(1) & 0xff == ord('q'):   # 1 is the time in ms
+        break
+
+
+
+
+
+print("Close vehicle object")
+vehicle.close()
+print('mission complete')
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
