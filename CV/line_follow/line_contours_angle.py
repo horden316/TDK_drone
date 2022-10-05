4796ca7a3d1e792275fd81da239fa3f9a3b5efcf
Ericgit310 <107010605+Ericgit310@users.noreply.github.com>
2022-10-05 17:44:26 +0800

    add contour area threshold

--- CV/line_follow/line_contours_angle.py
+++ CV/line_follow/line_contours_angle.py
@@ -51,60 +51,62 @@ while True:
              (center_x, center_y+cross_size), (0, 0, 255), 1)
     cv2.line(frame, (center_x-cross_size, center_y),
              (center_x+cross_size, center_y), (0, 0, 255), 1)
-    if len(contours) > 0:
+    for pic, contour in enumerate(contours):
         c = max(contours, key=cv2.contourArea)
         M = cv2.moments(c)
-        blackbox = cv2.minAreaRect(c)
-        (x_min, y_min), (w_min, h_min), angle = blackbox
-        box = cv2.boxPoints(blackbox)
-        box = np.int0(box)
-        cv2.drawContours(frame, [box], 0, (0, 0, 255), 3)
-        if angle < -45:
-            angle = 90 + angle
-        if w_min < h_min and angle > 0:
-            angle = (90 - angle) * -1
-        if w_min > h_min and angle < 0:
-            angle = 90 + angle
-        print("Angle:" + str(angle))
-        cv2.putText(frame, "Angle: " + str(angle), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (201, 194, 9), 1,
-                    cv2.LINE_AA)
-        if M["m00"] != 0:
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            print("X : "+str(cx)+" Y : "+str(cy))
-            x_distance = center[0]-cx
-            y_distance = center[1]-cy
-            Error = x_distance
-            total_Err = total_Err + Error
-            output = -(Kp*Error + Ki*total_Err + Kd * (Error - last_Err))
-            last_Error = Error
-            u = output
-            roll_angle = u*0.2
-            if roll_angle > 15:
-                roll_angle = 15
-            if roll_angle < -15:
-                roll_angle = -15
-            # roll_angle = PID(Error=x_distance, Kp=0.8, Ki=0,
-            #                  Kd=0, max_angle=15, a=0.2)
-            # pitch_angle = PID(Error=y_distance, Kp=0.8, Ki=0,
-            #                   Kd=0, max_angle=15, a=0.2)
-            # if cx >= 120 :
-            #     print("Turn Left")
-            # if cx < 120 and cx > 40 :
-            #     print("On Track!")
-            # if cx <=40 :
-            #     print("Turn Right")
-            # centroid circle
-            cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
-            # centroid line
-            cv2.line(frame,  center, (cx, cy), (0, 255, 255), 1)
-            # BGR
-            cv2.line(frame,  center, (cx, cy), (0, 255, 255), 1)
-            distance = distanceCalculate(center, (cx, cy))
-            cv2.putText(frame, "Distance: " + str(distance), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (201, 194, 9), 1,
+        area = cv2.contourArea(contour)
+        if (area > 1000):
+            blackbox = cv2.minAreaRect(c)
+            (x_min, y_min), (w_min, h_min), angle = blackbox
+            box = cv2.boxPoints(blackbox)
+            box = np.int0(box)
+            cv2.drawContours(frame, [box], 0, (0, 0, 255), 3)
+            if angle < -45:
+                angle = 90 + angle
+            if w_min < h_min and angle > 0:
+                angle = (90 - angle) * -1
+            if w_min > h_min and angle < 0:
+                angle = 90 + angle
+            print("Angle:" + str(angle))
+            cv2.putText(frame, "Angle: " + str(angle), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (201, 194, 9), 1,
                         cv2.LINE_AA)
-            cv2.putText(frame, "roll: " + str(roll_angle), (0, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (201, 194, 9), 1,
-                        cv2.LINE_AA)
+            if M["m00"] != 0:
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                print("X : "+str(cx)+" Y : "+str(cy))
+                x_distance = center[0]-cx
+                y_distance = center[1]-cy
+                Error = x_distance
+                total_Err = total_Err + Error
+                output = -(Kp*Error + Ki*total_Err + Kd * (Error - last_Err))
+                last_Error = Error
+                u = output
+                roll_angle = u*0.2
+                if roll_angle > 15:
+                    roll_angle = 15
+                if roll_angle < -15:
+                    roll_angle = -15
+                # roll_angle = PID(Error=x_distance, Kp=0.8, Ki=0,
+                #                  Kd=0, max_angle=15, a=0.2)
+                # pitch_angle = PID(Error=y_distance, Kp=0.8, Ki=0,
+                #                   Kd=0, max_angle=15, a=0.2)
+                # if cx >= 120 :
+                #     print("Turn Left")
+                # if cx < 120 and cx > 40 :
+                #     print("On Track!")
+                # if cx <=40 :
+                #     print("Turn Right")
+                # centroid circle
+                cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
+                # centroid line
+                cv2.line(frame,  center, (cx, cy), (0, 255, 255), 1)
+                # BGR
+                cv2.line(frame,  center, (cx, cy), (0, 255, 255), 1)
+                distance = distanceCalculate(center, (cx, cy))
+                cv2.putText(frame, "Distance: " + str(distance), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (201, 194, 9), 1,
+                            cv2.LINE_AA)
+                cv2.putText(frame, "roll: " + str(roll_angle), (0, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (201, 194, 9), 1,
+                            cv2.LINE_AA)
 
     else:
         print("I don't see the line")
