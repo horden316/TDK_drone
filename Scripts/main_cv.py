9deebdd00dc8256c0ff4222c8f5107c8fc171413
Ericgit310 <107010605+Ericgit310@users.noreply.github.com>
2022-10-12 02:59:14 +0800

    Update main_cv.py

--- Scripts/main_cv.py
+++ Scripts/main_cv.py
@@ -5,12 +5,14 @@ import time
 from log import log
 from detection import *
 # from control import *  # 會直接與mavlink連接
+from movement import *
 import math
-section = 1
-# cap = cv2.VideoCapture(
-#     "C:\\Users\\ericn\\Desktop\\TDK26\\TDK_drone\\video_detect\\video.mp4")
-cap = cv2.VideoCapture(0)
+section = 0
+cap = cv2.VideoCapture(
+    "C:\\Users\\ericn\\Desktop\\TDK26\\TDK_drone\\video_detect\\video.mp4")
+# cap = cv2.VideoCapture(0)
 ##############狀態參數##############
+drop_cnt = 0
 status = None
 red_count = 0
 thrust = 0
@@ -28,10 +30,18 @@ blue_upper = np.array([120, 255, 255])
 line_mask = 0
 blue_mask = 0
 red_mask = 0
+# blue h mask
+blue_h_lower = np.array([94, 80, 2])
+blue_h_upper = np.array([120, 255, 255])
 # LOG 錄製
 fourcc = cv2.VideoWriter_fourcc(*'XVID')  # 指定影像編碼方式
 out = cv2.VideoWriter("output"+str(int(time.time())) +
                       ".avi", fourcc, 20.0, (480,  360))
+# create white frame
+white = np.zeros([120, 160, 3], dtype=np.uint8)
+white.fill(255)
+# create white line frame
+line = cv2.rectangle(white, (60, 0), (100, 120), (0, 0, 0), -1)
 ##################起飛準備##################
 while (1):
     ret, frame = cap.read()
@@ -51,10 +61,15 @@ while (1):
     if section == 0:
         status = "takeoff_forward"
         print("takeoff_forward")
+        hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        out_mask = cv2.inRange(hsvFrame, blue_h_lower, blue_h_upper)
+        frame[out_mask > 0] = (255, 255, 255)
+        cv2.imshow("frame1", frame)
         (lx, ly), line_angle, line_frame, line_mask, line_x_dis, line_y_dis = line_detect(
-            frame=frame, draw_frame=draw_frame, line_mask=h)
+            frame=frame, draw_frame=draw_frame, line_mask=h, c_area=1000)
         # def takeoff () return thrust
         # def moveforward
+        section = 2
 
     #########################section1#########################
     #走線 + 紅燈辨識
@@ -87,21 +102,42 @@ while (1):
     #########################section2#########################
     # 走線 + 投遞:
     if section == 2:
+        drop, mask_blue, blue_mask, draw_frame, (bx, by), x_distance, y_distance = drop_detect(
+            frame, draw_frame, blue_lower=blue_lower, blue_upper=blue_upper)
+        mask_blue = cv2.bitwise_not(mask_blue)
+        frame[mask_blue > 0] = (255, 255, 255)
         (lx, ly), line_angle, line_frame, line_mask, line_x_dis, line_y_dis = line_detect(
             frame=frame, draw_frame=draw_frame, line_mask=50)
-        drop, blue_mask, draw_frame, (bx, by), x_distance, y_distance = drop_detect(
-            frame, draw_frame, blue_lower=blue_lower, blue_upper=blue_upper)
+        number_of_blue_pix = np.sum(mask_blue == 255)
+        print("blue pix=" + str(number_of_blue_pix))
+        mask_blue = cv2.bitwise_not(mask_blue)
+        frame[mask_blue > 0] = (255, 255, 255)
+        if (number_of_blue_pix < 18000):
+            line_angle = 100000000000
+        print(drop_cnt)
         if drop == True:
-            print("stay & drop")
+            drop_cnt += 1
+            pitch_angle, roll_angle, yaw_angle, thrust, status = stay(
+                x=bx, y=by, current_alt=0, angle=None, current_yaw=0, thrust=0.5)
+            if (drop_cnt > 100):
+                target = "drop"
+                target_xy = (bx, by)
+                # 校正位置
+                pitch_angle, roll_angle, yaw_angle, thrust, status = stay(
+                    x=bx, y=by, current_alt=0, angle=None, current_yaw=0, thrust=0.5)
+                # 拋物
+                # servo(servo_open=True)
+                print("丟丟丟丟丟丟丟丟丟丟丟丟丟丟丟丟丟丟丟丟丟丟丟丟丟丟丟")
         else:
-            print("move forward")
+            pitch_angle, roll_angle, yaw_angle, status, thrust = move_forward(
+                x=lx, current_alt=0, angle=line_angle, move_pitch_angle=-1, stay_pitch_angle=0, current_yaw=0, thrust=0.5)
     #########################section3#########################
     # if section==3:
     # if section==4:
     ######################### SET ALTITUDE #########################
     back_frame = log(frame=draw_frame, lane_mask=line_mask, red_mask=red_mask,
                      drop_mask=blue_mask, h_mask=0, ex_frame=0, lane_xy=(lx, ly), lane_angle=line_angle,
-                     lane_dis=line_x_dis, status=status, section=section, thrust=thrust)
+                     lane_dis=line_x_dis, status=status, section=section, thrust=drop_cnt)
 
     # log(frame=(100, 0, 0), lane_mask=(100, 0, 0), red_mask=(100, 0, 0), drop_mask=(100, 0, 0), h_mask=(100, 0, 0), ex_frame=(100, 0, 0),
     #     show=True, alt=0.0, pitch=0.0, roll=0.0, yaw=0.0,
